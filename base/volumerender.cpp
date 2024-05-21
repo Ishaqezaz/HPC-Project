d87e970c32da19b89f53bf3fae185f17336619ec
Ishaq <isez9805@gmail.com>
2024-05-21 20:39:27 +0200

    interpolate func

--- base/volumerender.cpp
+++ base/volumerender.cpp
@@ -46,13 +46,40 @@ vector<MatrixXf> HDF5Loading(const string& filename, const string& datasetName){
     return datacube;
 }
 
-Vector4d transferFunction(double x) {
+Vector4d transferFunction(double x){
     double r = 1.0 * exp(-(x - 9.0) * (x - 9.0) / 1.0) + 0.1 * exp(-(x - 3.0) * (x - 3.0) / 0.1) + 0.1 * exp(-(x + 3.0) * (x + 3.0) / 0.5);
     double g = 1.0 * exp(-(x - 9.0) * (x - 9.0) / 1.0) + 1.0 * exp(-(x - 3.0) * (x - 3.0) / 0.1) + 0.1 * exp(-(x + 3.0) * (x + 3.0) / 0.5);
     double b = 0.1 * exp(-(x - 9.0) * (x - 9.0) / 1.0) + 0.1 * exp(-(x - 3.0) * (x - 3.0) / 0.1) + 1.0 * exp(-(x + 3.0) * (x + 3.0) / 0.5);
     double a = 0.6 * exp(-(x - 9.0) * (x - 9.0) / 1.0) + 0.1 * exp(-(x - 3.0) * (x - 3.0) / 0.1) + 0.01 * exp(-(x + 3.0) * (x + 3.0) / 0.5);
     
     return Vector4d(r, g, b, a);
+}
+
+double trilinearInterpolate(const std::vector<double>& data, int Nx, int Ny, int Nz, double x, double y, double z){
+    int x0 = floor(x);
+    int x1 = x0 + 1;
+    int y0 = floor(y);
+    int y1 = y0 + 1;
+    int z0 = floor(z);
+    int z1 = z0 + 1;
+
+    if(x1 >= Nx) x1 = Nx - 1;
+    if(y1 >= Ny) y1 = Ny - 1;
+    if(z1 >= Nz) z1 = Nz - 1;
+
+    double xd = x - x0;
+    double yd = y - y0;
+    double zd = z - z0;
+
+    double c00 = data[x0 * Ny * Nz + y0 * Nz + z0] * (1 - xd) + data[x1 * Ny * Nz + y0 * Nz + z0] * xd;
+    double c01 = data[x0 * Ny * Nz + y0 * Nz + z1] * (1 - xd) + data[x1 * Ny * Nz + y0 * Nz + z1] * xd;
+    double c10 = data[x0 * Ny * Nz + y1 * Nz + z0] * (1 - xd) + data[x1 * Ny * Nz + y1 * Nz + z0] * xd;
+    double c11 = data[x0 * Ny * Nz + y1 * Nz + z1] * (1 - xd) + data[x1 * Ny * Nz + y1 * Nz + z1] * xd;
+
+    double c0 = c00 * (1 - yd) + c10 * yd;
+    double c1 = c01 * (1 - yd) + c11 * yd;
+
+    return c0 * (1 - zd) + c1 * zd;
 }
 
 int main(){
